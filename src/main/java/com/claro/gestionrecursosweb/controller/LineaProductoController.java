9f95d3d9eec0444ef16812ac60ee02079807a84f
andrespperdomo <andrespperdomo@gmail.com>
2020-02-25 09:03:58 -0500

    Ajustes linea producto

--- src/main/java/com/claro/gestionrecursosweb/controller/LineaProductoController.java
+++ src/main/java/com/claro/gestionrecursosweb/controller/LineaProductoController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.claro.gestionrecursosweb.controller;
 
 import java.util.Date;
@@ -93,99 +93,5 @@ public class LineaProductoController extends BaseController {
 	
 		
 }
-=======
-package com.claro.gestionrecursosweb.controller;
 
-import java.util.Date;
-import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.claro.gestionrecursosweb.domain.ApiService;
-import com.claro.gestionrecursosweb.dto.LineaProductoDto;
-import com.claro.gestionrecursosweb.dto.PersonaDto;
-import com.claro.gestionrecursosweb.dto.ProveedorDto;
-
-@Controller
-@RequestMapping("/LineaProducto")
-public class LineaProductoController extends BaseController {
-	
-	@Value("${claro.dominio.lineaproducto.nombre}")
-	private String dominio;
-	
-	@Autowired
-	private ApiService<LineaProductoDto, Integer> service;
-
-	public void configurarService() {
-		service.setapiservicename(dominio);
-	}
-	
-	@GetMapping("/filtro")
-	public String filtro(Model model) {
-		configurarService();
-		Iterable<LineaProductoDto> dto = service.findAll(LineaProductoDto.class);
-		model.addAttribute("modelo", dto);		
-		System.out.println(model.toString());
-		
-		return  dominio + "/LineaProducto";
-	}
-	
-	@GetMapping("Crear")
-	public String crear(Model modelo) {
-		configurarService();
-		modelo.addAttribute("cl_formaction", "Crear");
-		modelo.addAttribute("modelo", new LineaProductoDto());
-		return dominio + "/CrearLineaProducto";
-	}
-	
-	@PostMapping("/Crear")
-	public String crear(LineaProductoDto dto, BindingResult result, Model modelo, HttpServletRequest request) {
-		configurarService();
-		modelo.addAttribute("cl_formaction", "Crear");
-		dto.setFechacreacion(new Date());
-		dto.setFechamodificacion(new Date());
-		LineaProductoDto dtoResultado = service.insert(dto, LineaProductoDto.class);
-		return redireccion("Editar", dtoResultado.getId().toString(), "S", "C", request);
-	}
-	
-	@GetMapping("/Editar/{id}")
-	public String editar(@PathVariable Integer id, Model modelo, @RequestParam(required = false) String cla) {
-		configurarService();
-		mostrarMensajes(modelo, cla);		
-		modelo.addAttribute("cl_formaction", "Editar");
-		
-		Optional<LineaProductoDto> dtoResultado = service.findById(id, LineaProductoDto.class);
-		System.out.println("########### valores "+dtoResultado.toString());
-		if (dtoResultado == null)
-			dtoResultado = Optional.of(new LineaProductoDto());
-				
-		modelo.addAttribute("modelo", dtoResultado.get());
-		return dominio + "/CrearLineaProducto";
-	}
-	
-	@PostMapping("/Editar")
-	public String editar(LineaProductoDto dto, BindingResult result, Model modelo) {
-		configurarService();		
-		modelo.addAttribute("cl_formaction", "Editar");
-		dto.setFechamodificacion(new Date());
-		LineaProductoDto dtoResultado = service.update(dto.getId(), dto, LineaProductoDto.class);				
-		modelo.addAttribute("modelo", dtoResultado);
-		
-		mostrarMensajes(modelo, "S", "U");
-		return dominio + "/CrearLineaProducto";
-	}
-	
-		
-}
->>>>>>> 2a2d560f5ec48a25884b391a3a784b9e7f87d4f0
--- src/main/java/com/claro/gestionrecursosweb/dto/LineaProductoDto.java
+++ src/main/java/com/claro/gestionrecursosweb/dto/LineaProductoDto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.claro.gestionrecursosweb.dto;
 
 import java.sql.Timestamp;
@@ -72,82 +71,6 @@ public class LineaProductoDto {
 				+ simpleDateFormat + ", fechaCreacionString=" + fechaCreacionString + ", fechaModificacionString="
 				+ fechaModificacionString + "]";
 	}
-	
+		
 }
-=======
-package com.claro.gestionrecursosweb.dto;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class LineaProductoDto {
-	
-	private Integer id;
-
-	private Date fechacreacion;
-
-	private Date fechamodificacion;
-
-	private String nombre;
-	
-	public String incorrectData;
-	
-	SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy");
-	@JsonIgnore
-	public String fechaCreacionString;
-	@JsonIgnore
-	public String fechaModificacionString;
-	
-	
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Date getFechacreacion() {
-		return fechacreacion;
-	}
-	
-	public void setFechacreacion(Date fechacreacion) {
-		this.fechacreacion = fechacreacion;
-	}
-
-	public Date getFechamodificacion() {
-		return fechamodificacion;
-	}
-
-	public void setFechamodificacion(Date fechamodificacion) {
-		this.fechamodificacion = fechamodificacion;
-	}
-	public String getFechaCreacionString() {
-		return simpleDateFormat.format(fechacreacion);
-	}
-	public String getFechaModificacionString() {
-		return simpleDateFormat.format(fechamodificacion);
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	@Override
-	public String toString() {
-		return "LineaProductoDto [id=" + id + ", fechacreacion=" + fechacreacion + ", fechamodificacion="
-				+ fechamodificacion + ", nombre=" + nombre + ", incorrectData=" + incorrectData + ", simpleDateFormat="
-				+ simpleDateFormat + ", fechaCreacionString=" + fechaCreacionString + ", fechaModificacionString="
-				+ fechaModificacionString + "]";
-	}
-	
-}
->>>>>>> 2a2d560f5ec48a25884b391a3a784b9e7f87d4f0
